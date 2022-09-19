b1172b21e924124a01ed90ef07d1f81c68463142
neneprm <s.prima@hotmail.com>
2022-09-19 23:38:55 +0700

    Set global stylesheet

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {
@@ -11,20 +11,23 @@ module.exports = {
       },
     },
   },
-  plugins: [require('@tailwindcss/typography'), require("daisyui")],
+  plugins: [require('@tailwindcss/typography'), require('daisyui')],
   daisyui: {
     themes: [
       {
         mytheme: {
-          "primary": "#DF6F3A",
-          "secondary": "#FBA94F",
-          "accent": "#429EBD",
-          "neutral": "#585859",
-          "base-100": "#FFFFFF",
-          "info": "#60A5FA",
-          "success": "#10B981",
-          "warning": "#FFCA40",
-          "error": "#E25E4C",
+          'primary': '#DF6F3A',
+          'primary-focus': '#D35A22',
+          'secondary': '#FBA94F',
+          'secondary-focus': '#FB9C37',
+          'accent': '#429EBD',
+          'accent-focus': '#3A8BA6',
+          'neutral': '#585859',
+          'base-100': '#FFFFFF',
+          'info': '#60A5FA',
+          'success': '#10B981',
+          'warning': '#FFCA40',
+          'error': '#E25E4C',
         },
       },
     ],
