09382223629e4c62e4d9b463c62f4ab6d4402f68
neneprm <s.prima@hotmail.com>
2022-10-07 04:50:18 +0700

    Create footer

---
+++ pages/components/footer.tsx
@@ -0,0 +1,12 @@
+const Footer = () => {
+    return (
+        <footer className="footer py-10 bg-neutral text-neutral-content">
+            <div className="mx-0 lg:mx-48">
+                <h1 className="sm:text-xl md:text-3xl lg:text-5xl text-primary font-bold">KMITL</h1>
+                <p>© 2022 International College, King Mongkut's Institute of Technology Ladkrabang</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
