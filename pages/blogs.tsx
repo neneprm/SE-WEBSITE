7536504c8c9ca227229b6e1d796ed42dfa4152ec
neneprm <s.prima@hotmail.com>
2022-10-07 04:51:43 +0700

    Create title for each page

--- pages/blogs.tsx
+++ pages/blogs.tsx
@@ -1,7 +1,30 @@
+interface IHeader {
+    text: string,
+    style: string
+  }
+  
 const Blogs = () => {
+    const H3: React.FunctionComponent<IHeader> = ({ style, text }) => {
+        return (
+          <h3 className={`text-lg sm:text-xl md:text-2xl lg:text-3xl ${style}`}>{text}</h3>
+        )
+      }
+      const H4: React.FunctionComponent<IHeader> = ({ style, text }) => {
+        return (
+          <h4 className={`text-base sm:text-lg md:text-xl lg:text-2xl ${style}`}>{text}</h4>
+        )
+      }
+      const P: React.FunctionComponent<IHeader> = ({ style, text }) => {
+        return (
+          <p className={`text-sm sm:text-base md:text-lg lg:text-xl ${style}`}>{text}</p>
+        )
+      }
+      
     return (
         <div>
-            
+            <div className="container mx-auto">
+                <H3 style="text-gradient font-bold text-center" text="BLOGS"/>
+            </div>
         </div>
     )
 }
