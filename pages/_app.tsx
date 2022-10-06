cae3f5c96c2bc32187ca2f008c8e11f3f2847b91
neneprm <s.prima@hotmail.com>
2022-10-07 01:54:06 +0700

    Fix default app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,14 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import NavBar from './components/navbar'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className="bg-base-100 font-Nunito mx-0 lg:mx-48">
+      <NavBar />
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
