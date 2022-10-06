4cb76843ac0f5bc51e7edae9b22e01e6c930f490
neneprm <s.prima@hotmail.com>
2022-10-07 04:52:42 +0700

    Create footer

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -1,9 +1,9 @@
 const Footer = () => {
     return (
-        <footer className="footer py-10 bg-neutral text-neutral-content">
+        <footer className="footer-center md:footer lg:footer py-10 bg-neutral text-neutral-content">
             <div className="mx-0 lg:mx-48">
-                <h1 className="sm:text-xl md:text-3xl lg:text-5xl text-primary font-bold">KMITL</h1>
-                <p>© 2022 International College, King Mongkut's Institute of Technology Ladkrabang</p>
+                <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-primary font-bold">KMITL</h1>
+                <p className="text-sm sm:text-base md:text-lg lg:text-xl">© 2022 International College, King Mongkut's Institute of Technology Ladkrabang</p>
             </div>
         </footer>
     )
