666bf08e73e4931bf64c062f2c5e30ef33481554
neneprm <s.prima@hotmail.com>
2022-10-09 22:11:48 +0700

    Fix link and card in program

--- pages/program.tsx
+++ pages/program.tsx
@@ -1,48 +1,24 @@
 import Link from "next/link"
 
-import {H3} from "./components/headers"
-import { FaScroll, FaGraduationCap, FaCalendar, FaPlane, FaSuitcase} from "react-icons/fa";
+import { H3 } from "./components/headers"
+import Card from "./components/card";
+
+import { FaScroll, FaGraduationCap, FaCalendar, FaPlane, FaSuitcase } from "react-icons/fa";
 import { BsPeopleFill } from "react-icons/bs";
 
-interface ICard {
-  title: string,
-  link: string
-  icon: JSX.Element
-}
-
 const Program = () => {
-  const Card: React.FunctionComponent<ICard> = ({ title, icon, link }) => {
-    return (
-      <div className="card w-96 bg-base-100 card-bordered">
-        <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
-        <div className="card-body">
-          <h2 className="card-title">
-            <span>
-              {icon}
-            </span>
-            {title}</h2>
-          <div className="card-actions justify-end">
-            <button className="btn btn-accent text-base-100">
-              <Link href={`/program/${link}`}><a>Read more →</a></Link>
-            </button>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
   return (
     <div>
       <div className="container mx-auto">
         <H3 style="text-gradient font-bold text-center" text="PROGRAM" />
       </div>
       <div className="grid md:grid-cols-2 lg:grid-cols-3">
-        <Card title="Curriculum" icon={<FaScroll className="text-primary"/>} link="curriculum" />
-        <Card title="KMITL-Glasgow" icon={<FaGraduationCap className="text-primary"/>} link="kmitl-glasgow" />
-        <Card title="Study Plans" icon={<FaCalendar className="text-primary"/>} link="study-plans" />
-        <Card title="Exchange Study Abroad" icon={<FaPlane className="text-primary"/>} link="exchange-study-abroad" />
-        <Card title="Internships" icon={<FaSuitcase className="text-primary"/>} link="internships" />
-        <Card title="Lecturers" icon={<BsPeopleFill className="text-primary"/>} link="lecturers" />
+        <Card image={<figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>} title="Curriculum" icon={<FaScroll className="text-primary" />} link="/program/curriculum" />
+        <Card image={<figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>} title="KMITL-Glasgow" icon={<FaGraduationCap className="text-primary" />} link="/program/kmitl-glasgow" />
+        <Card image={<figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>} title="Study Plans" icon={<FaCalendar className="text-primary" />} link="/program/study-plans" />
+        <Card image={<figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>} title="Exchange Study Abroad" icon={<FaPlane className="text-primary" />} link="/program/exchange-study-abroad" />
+        <Card image={<figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>} title="Internships" icon={<FaSuitcase className="text-primary" />} link="/program/internships" />
+        <Card image={<figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>} title="Lecturers" icon={<BsPeopleFill className="text-primary" />} link="/program/lecturers" />
       </div>
     </div>
   )
