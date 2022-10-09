79b4a8db945fa763d485747f54d2b4e42fa1e0a1
neneprm <s.prima@hotmail.com>
2022-10-09 21:34:33 +0700

    Delete headers

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,53 +6,9 @@ import Link from 'next/link'
 import hero from './assets/hero.svg'
 
 import PrimaryButton from './components/primary_button'
-
-interface IHeader {
-  text: string,
-  style: string
-}
-
-interface ICard {
-  title: string,
-  body: string,
-  action: string
-}
+import {H1, H3, H4, P, Card} from './components/headers'
 
 const Home: NextPage = () => {
-  const H1: React.FunctionComponent<IHeader> = ({ style, text }) => {
-    return (
-      <h1 className={`text-2xl sm:text-3xl md:text-4xl lg:text-5xl ${style}`}>{text}</h1>
-    )
-  }
-  const H3: React.FunctionComponent<IHeader> = ({ style, text }) => {
-    return (
-      <h3 className={`text-lg sm:text-xl md:text-2xl lg:text-3xl ${style}`}>{text}</h3>
-    )
-  }
-  const H4: React.FunctionComponent<IHeader> = ({ style, text }) => {
-    return (
-      <h4 className={`text-base sm:text-lg md:text-xl lg:text-2xl ${style}`}>{text}</h4>
-    )
-  }
-  const P: React.FunctionComponent<IHeader> = ({ style, text }) => {
-    return (
-      <p className={`text-sm sm:text-base md:text-lg lg:text-xl ${style}`}>{text}</p>
-    )
-  }
-  const Card: React.FunctionComponent<ICard> = ({ title, body, action }) => {
-    return (
-      <div className="card w-96 bg-base-100 card-bordered">
-        <div className="card-body">
-          <h2 className="card-title">{title}</h2>
-          <p>{body}</p>
-          <div className="card-actions justify-end">
-            <button className="btn">{action}</button>
-          </div>
-        </div>
-      </div>
-    )
-  }
-
   return (
     <>
       <section className="hero min-h-screen bg-base-100">
@@ -62,8 +18,8 @@ const Home: NextPage = () => {
             <H1 style="font-bold text-gradient" text="SOFTWARE" />
             <H1 style="font-bold mb-6" text="ENGINEERING" />
             <H3 style="mb-3" text="Bachelor of Engineering" />
-            <H4 style="" text="Become professional software developers" />
-            <H4 style="" text="Change the world with lines of code" />
+            <H4 text="Become professional software developers" />
+            <H4 text="Change the world with lines of code" />
           </span>
         </div>
       </section>
@@ -79,15 +35,13 @@ const Home: NextPage = () => {
       </div> */}
 
       <H4 style="font-bold text-gradient" text="What is Software Engineering?" />
-      <section className="drop-shadow-lg">
-        <div className="card w-full bg-base-100 card-bordered">
-          <div className="card-body font-normal">
-            <P style="" text="Software engineering (SE) is an engineering discipline concerning all aspects of software production, including software analysis, design, development, testing, and deployment. SE requires profound abstract and logical thinking and the application of mathematics, logic, and computer science in order to produce efficient and reliable software with the available resources."/>
-            <div className="card-actions justify-end">
-              <button className="btn btn-accent">
-                <Link href={"/about"}><a>Read more →</a></Link>
-              </button>
-            </div>
+      <section className="card w-full bg-base-100 card-bordered shadow-lg">
+        <div className="card-body font-normal">
+          <P style="" text="Software engineering (SE) is an engineering discipline concerning all aspects of software production, including software analysis, design, development, testing, and deployment. SE requires profound abstract and logical thinking and the application of mathematics, logic, and computer science in order to produce efficient and reliable software with the available resources." />
+          <div className="card-actions justify-end">
+            <button className="btn btn-accent text-base-100">
+              <Link href={"/about"}><a>Read more →</a></Link>
+            </button>
           </div>
         </div>
       </section>
--- pages/program.tsx
+++ pages/program.tsx
@@ -1,32 +1,51 @@
-interface IHeader {
-    text: string,
-    style: string
-  }
+import Link from "next/link"
+
+import {H3} from "./components/headers"
+import { FaScroll, FaGraduationCap, FaCalendar, FaPlane, FaSuitcase} from "react-icons/fa";
+import { BsPeopleFill } from "react-icons/bs";
+
+interface ICard {
+  title: string,
+  link: string
+  icon: JSX.Element
+}
 
 const Program = () => {
-    const H3: React.FunctionComponent<IHeader> = ({ style, text }) => {
-        return (
-          <h3 className={`text-lg sm:text-xl md:text-2xl lg:text-3xl ${style}`}>{text}</h3>
-        )
-      }
-      const H4: React.FunctionComponent<IHeader> = ({ style, text }) => {
-        return (
-          <h4 className={`text-base sm:text-lg md:text-xl lg:text-2xl ${style}`}>{text}</h4>
-        )
-      }
-      const P: React.FunctionComponent<IHeader> = ({ style, text }) => {
-        return (
-          <p className={`text-sm sm:text-base md:text-lg lg:text-xl ${style}`}>{text}</p>
-        )
-      }
-      
+  const Card: React.FunctionComponent<ICard> = ({ title, icon, link }) => {
     return (
-        <div>
-            <div className="container mx-auto">
-                <H3 style="text-gradient font-bold text-center" text="PROGRAM"/>
-            </div>
+      <div className="card w-96 bg-base-100 card-bordered">
+        <figure><img src="https://placeimg.com/400/225/arch" alt="Shoes" /></figure>
+        <div className="card-body">
+          <h2 className="card-title">
+            <span>
+              {icon}
+            </span>
+            {title}</h2>
+          <div className="card-actions justify-end">
+            <button className="btn btn-accent text-base-100">
+              <Link href={`/program/${link}`}><a>Read more →</a></Link>
+            </button>
+          </div>
         </div>
+      </div>
     )
+  }
+
+  return (
+    <div>
+      <div className="container mx-auto">
+        <H3 style="text-gradient font-bold text-center" text="PROGRAM" />
+      </div>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3">
+        <Card title="Curriculum" icon={<FaScroll className="text-primary"/>} link="curriculum" />
+        <Card title="KMITL-Glasgow" icon={<FaGraduationCap className="text-primary"/>} link="kmitl-glasgow" />
+        <Card title="Study Plans" icon={<FaCalendar className="text-primary"/>} link="study-plans" />
+        <Card title="Exchange Study Abroad" icon={<FaPlane className="text-primary"/>} link="exchange-study-abroad" />
+        <Card title="Internships" icon={<FaSuitcase className="text-primary"/>} link="internships" />
+        <Card title="Lecturers" icon={<BsPeopleFill className="text-primary"/>} link="lecturers" />
+      </div>
+    </div>
+  )
 }
 
 export default Program
