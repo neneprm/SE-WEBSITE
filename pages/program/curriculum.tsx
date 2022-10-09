7d7ca69de13225abe5702ba533d7c45b55e89ec7
neneprm <s.prima@hotmail.com>
2022-10-09 22:12:03 +0700

    Crate subpage example

---
+++ pages/program/curriculum.tsx
@@ -0,0 +1,13 @@
+import {H3} from "../components/headers"
+
+const Curriculum = () => { 
+    return (
+        <div>
+            <div className="container mx-auto">
+                <H3 style="text-gradient font-bold text-center" text="CURRICULUM"/>
+            </div>
+        </div>
+    )
+}
+
+export default Curriculum
