eaff0aff8a424edc51bb17e31147330f6b2b607d
faynch <83182929+faynch@users.noreply.github.com>
2022-10-25 21:24:04 +0700

    remove btn from curriculum page

---
+++ pages/components/card_content.tsx
@@ -0,0 +1,25 @@
+import {P} from "./headers"
+
+interface ICard {
+    cardStyle?: string,
+    image?: JSX.Element
+    icon?: JSX.Element,
+    titleStyle?: string,
+    title?: string,
+    body?: string
+}
+
+const CardContent: React.FunctionComponent<ICard> = ({ cardStyle, image, icon, titleStyle, title, body}) => {
+    return (
+        <div className={`card bg-base-100 card-bordered shadow-lg ${cardStyle}`}>
+            {image}
+            <div className="card-body">
+                <h2 className={`card-title ${titleStyle}`}>
+                    <span>{icon}</span>{title}</h2>
+                <P style="pl-3" text={`${body}`} />
+            </div>
+        </div>
+    )
+}
+
+export default CardContent
