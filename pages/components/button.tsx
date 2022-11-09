a92875e4d97cbbea12f23f82116ef3732c3c2c40
neneprm <s.prima@hotmail.com>
2022-11-10 03:27:25 +0700

    Fix button

--- pages/components/button.tsx
+++ pages/components/button.tsx
@@ -1,18 +1,21 @@
-import { useState } from 'react';
-import Link from 'next/link';
+import { useState } from "react";
+import Link from "next/link";
 
 interface IButton {
-    style: string,
-    link: string,
-    text: string,
-
+  style: string,
+  link: string,
+  text: string
 }
 
 const Button: React.FunctionComponent<IButton> = ({ style, link, text }) => {
-    return (
-        <button className={`btn btn-ghost text-sm sm:text-base md:text-lg lg:text-xl font-bold mx-2 ${style}`}>
-            <Link href={`${link}`}><a>{text}</a></Link>
-        </button>
-    )
+  return (
+    <Link href={`${link}`}>
+      <button
+        className={`btn btn-ghost text-sm sm:text-base md:text-lg lg:text-xl font-bold mx-2 ${style}`}
+      >
+        <a>{text}</a>
+      </button>
+    </Link>
+  )
 }
 export default Button
