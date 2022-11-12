ee7113d52a0c62be4a0148ded270ddce3a402e37
neneprm <s.prima@hotmail.com>
2022-11-13 04:18:01 +0700

    Fix text color

--- pages/program/internships.tsx
+++ pages/program/internships.tsx
@@ -8,7 +8,7 @@ const Internships = () => {
         <H3 style="text-gradient font-bold text-center" text="INTERNSHIPS" />
       </section>
       <section className="mb-8">
-        <H4 style="font-bold mb-4" text="Software Industrial Internships" />
+        <H4 style="font-bold mb-4 text-primary" text="Software Industrial Internships" />
         <P text="The Software Industrial Internship is a required component of the B.Eng. in Software Engineering program. The objectives are for the students to gain work experience in the software industry and to understand the role of a software engineer in business and the society. Every student is expected to undertake an internship position related to software development in a company in Thailand or abroad during the summer semester at the end of the second year of their study." />
       </section>
 
