89978f6a4c73c4a241e74af7d5bebafba1f616dc
neneprm <s.prima@hotmail.com>
2022-11-13 02:34:58 +0700

    Footer margin

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -1,14 +1,13 @@
+import { H1, P } from "./headers"
 const Footer = () => {
   return (
-    <footer className="footer-center md:footer lg:footer py-10 bg-neutral text-neutral-content">
-      <div className="mx-0 lg:mx-48">
-        <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl text-primary font-bold">
-          KMITL
-        </h1>
-        <p className="text-sm sm:text-base md:text-lg lg:text-xl">
-          © 2022 International College, King Mongkut's Institute of Technology
-          Ladkrabang
-        </p>
+    <footer className="footer-center md:footer py-10 md:py-20 bg-neutral text-neutral-content">
+      <div className="mx-4 md:mx-12">
+        <H1 style="text-primary font-bold" text="KMITL" />
+        <P
+          text="&copy; 2022 International College, King Mongkut's Institute of Technology
+          Ladkrabang"
+        />
       </div>
     </footer>
   )
