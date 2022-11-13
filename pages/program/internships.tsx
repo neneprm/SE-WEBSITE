c3c84b70a93e66b5573f3c5da5ae1b1bd4b1d861
neneprm <s.prima@hotmail.com>
2022-11-14 01:28:35 +0700

    Fix path and add glasgow info

--- pages/program/internships.tsx
+++ pages/program/internships.tsx
@@ -1,5 +1,6 @@
 import { H3, H4, P } from "../components/headers"
-import StudentInfo from "../components/internships/students-info"
+import KmitlStudentInfo from "../components/internships/kmitl-students-info"
+import GlasgowStudentInfo from "../components/internships/glasgow-students-info"
 
 const Internships = () => {
   return (
@@ -8,12 +9,16 @@ const Internships = () => {
         <H3 style="text-gradient font-bold text-center" text="INTERNSHIPS" />
       </section>
       <section className="mb-8">
-        <H4 style="font-bold mb-4 text-primary" text="Software Industrial Internships" />
+        <H4
+          style="font-bold mb-4 text-primary"
+          text="Software Industrial Internships"
+        />
         <P text="The Software Industrial Internship is a required component of the B.Eng. in Software Engineering program. The objectives are for the students to gain work experience in the software industry and to understand the role of a software engineer in business and the society. Every student is expected to undertake an internship position related to software development in a company in Thailand or abroad during the summer semester at the end of the second year of their study." />
       </section>
 
       <section className="mb-8 space-y-4">
-        <StudentInfo/>
+        <KmitlStudentInfo />
+        <GlasgowStudentInfo />
       </section>
     </>
   )
