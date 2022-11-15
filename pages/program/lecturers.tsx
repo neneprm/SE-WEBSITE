80ddeb2ce8d877dd6dd8e8d7b6b5d7c453ba26b7
neneprm <s.prima@hotmail.com>
2022-11-16 02:08:26 +0700

    Create lecturers page

---
+++ pages/program/lecturers.tsx
@@ -0,0 +1,15 @@
+import { H3 } from "../components/headers"
+
+const Lecturers = () => {
+  return (
+    <>
+      <section className="container mx-auto my-4">
+        <H3 style="text-gradient font-bold text-center" text="LECTURERS" />
+      </section>
+
+      <section>{/* Content */}</section>
+    </>
+  )
+}
+
+export default Lecturers
