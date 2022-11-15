131a9db92a4ddac012ccfee28133c6f4e95cf6e6
neneprm <s.prima@hotmail.com>
2022-11-16 02:02:52 +0700

    Fix spacing

--- pages/components/internships/companies-info.tsx
+++ pages/components/internships/companies-info.tsx
@@ -28,9 +28,9 @@ const CompaniesInfo = () => {
         </div>
         <div className="collapse-content">
           <P text="Internship is an integral part of the KMITL - Glasgow 2+2 joint study program. Every student joining the 2+2 program is required to undertake an internship to order to simultaneously fulfill the requirements for the following courses:" />
-          <ol className="mt-4 list-decimal list-inside text-sm sm:text-base md:text-lg lg:text-xl">
+          <ol className="mt-4 list-decimal list-inside text-sm sm:text-base md:text-lg lg:text-xl space-y-2">
             {/* 1. Requirement */}
-            <li className="mb-4 font-bold">Requirements</li>
+            <li className="font-bold">Requirements</li>
             {/* Duration and Timing */}
             <P style="font-bold" text="Duration and Timing" />
             <P text="The internship takes place in the summer semester of Year 2 of the program. The student is expected to work full-time for 7 - 10 weeks during late May to early August." />
@@ -72,8 +72,8 @@ const CompaniesInfo = () => {
               style="my-2"
               text="The following examples illustrate opposite ends of the spectrum of suitable work:"
             />
-            <ul className="ml-4 list-disc list-inside text-justify text-sm sm:text-base md:text-lg lg:text-xl">
-              <li className="my-2">
+            <ul className="ml-4 list-disc list-inside text-justify text-sm sm:text-base md:text-lg lg:text-xl space-y-2">
+              <li>
                 At the one end of the spectrum, a student working for a large
                 company undertakes small clearly-defined pieces of work within a
                 large project. The student must first make a significant effort
@@ -93,13 +93,13 @@ const CompaniesInfo = () => {
             <P text="Since the student is likely to be inexperienced and cannot be expected to work without supervision, the company should assign a supervisor to offer advice to the student and monitor the student's progress. The International College will also nominate an internship supervisor who is available for consultation to both the student and the company throughout the internship." />
 
             {/* 2. Assessments */}
-            <li className="mt-8 mb-4 font-bold">Assessments</li>
+            <li className="font-bold">Assessments</li>
             <P
               text="To assess the student's internship, we kindly ask the company to monitor the student's performance during their internship and complete the internship assessment form. The internship assessment form is a confidential document and should be returned directly to the International College after the student has completed their internship. The assessment by the company is a crucial component which will be used to assess the student."
             />
 
             {/* 3. Contact */}
-            <li className="mt-8 mb-4 font-bold">Contact</li>
+            <li className="font-bold">Contact</li>
             <P text="Should the company have any problem, question, or suggestion regarding the internship or would like to recruit our students for internships, please contact the people below:" />
             <div className="flex flex-col md:flex-row">
               <P style="font-bold md:mr-2" text="Internship advisor" />
