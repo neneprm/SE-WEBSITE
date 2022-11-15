9351d24c71dddd788e9753a9afb0ad2049be5d70
faynch <83182929+faynch@users.noreply.github.com>
2022-11-15 17:45:39 +0700

    initial study page

---
+++ pages/program/study-plans.tsx
@@ -0,0 +1,146 @@
+import Link from "next/link"
+import { H3, H4, P } from "../components/headers"
+
+const StudyPlans = () => {
+    return (
+        <>
+            <section className="container mx-auto my-4">
+                <H3
+                    style="text-gradient font-bold text-center"
+                    text="STUDY PLANS"
+                />
+            </section>
+
+            <section className="mb-8">
+                <H4
+                    style="text-primary font-bold"
+                    text="Bachelor of Engineering in Software Engineering "
+                /><br />
+                <div className="flex">
+                    <div className="flex-auto">
+                        <H4
+                            style="text-base"
+                            text="University"
+                        />
+                        <div className="flex">
+                            <label className="label cursor-pointer ">
+                                <input type="radio" name="university" className="radio checked:bg-primary mr-2" />
+                                <span className="label-text">KMITL</span>
+                            </label>
+                        </div>
+                        <div className="flex">
+                            <label className="label cursor-pointer">
+                                <input type="radio" name="university" className="radio checked:bg-primary mr-2" />
+                                <span className="label-text">Glasgow</span>
+                            </label>
+                        </div>
+                    </div>
+                    <div className="flex-auto">
+                        <H4
+                            style="text-base"
+                            text="Specializations"
+                        />
+                        <div className="flex">
+                            <label className="label cursor-pointer ">
+                                <input type="radio" name="specializations" className="radio checked:bg-primary mr-2" />
+                                <span className="label-text">Enterprise SE</span>
+                            </label>
+                        </div>
+                        <div className="flex">
+                            <label className="label cursor-pointer">
+                                <input type="radio" name="specializations" className="radio checked:bg-primary mr-2" />
+                                <span className="label-text">Internet of Things</span>
+                            </label>
+                        </div>
+                        <div className="flex">
+                            <label className="label cursor-pointer">
+                                <input type="radio" name="specializations" className="radio checked:bg-primary mr-2" />
+                                <span className="label-text">Intelligent Systems</span>
+                            </label>
+                        </div>
+                    </div>
+                    <div className="flex-auto">
+                        <H4
+                            style="text-base"
+                            text="Year"
+                        />
+                        <div className="flex">
+                            <label className="label cursor-pointer ">
+                                <input type="radio" name="year" className="radio checked:bg-primary mr-2" />
+                                <span className="label-text">Year 1</span>
+                            </label>
+                        </div>
+                        <div className="flex">
+                            <label className="label cursor-pointer">
+                                <input type="radio" name="year" className="radio checked:bg-primary mr-2" />
+                                <span className="label-text">Year 2</span>
+                            </label>
+                        </div>
+                        <div className="flex">
+                            <label className="label cursor-pointer ">
+                                <input type="radio" name="year" className="radio checked:bg-primary mr-2" />
+                                <span className="label-text">Year 3</span>
+                            </label>
+                        </div>
+                        <div className="flex">
+                            <label className="label cursor-pointer">
+                                <input type="radio" name="year" className="radio checked:bg-primary mr-2" />
+                                <span className="label-text">Year 4</span>
+                            </label>
+                        </div>
+                    </div>
+                    <div className="flex-auto">
+                        <H4
+                            style="text-base"
+                            text="Semester"
+                        />
+                        <div className="flex">
+                            <label className="label cursor-pointer ">
+                                <input type="radio" name="radio-10" className="radio checked:bg-primary mr-2" />
+                                <span className="label-text">Semester 1</span>
+                            </label>
+                        </div>
+                        <div className="flex">
+                            <label className="label cursor-pointer">
+                                <input type="radio" name="radio-10" className="radio checked:bg-primary mr-2" />
+                                <span className="label-text">Semester 2</span>
+                            </label>
+                        </div>
+                    </div>
+                    <div className="flex-auto">
+                        <H4
+                            style="text-base"
+                            text="Elective"
+                        />
+                        <div className="flex">
+                            <label className="label cursor-pointer ">
+                                <input type="radio" name="elective" className="radio checked:bg-primary mr-2" />
+                                <span className="label-text">Major Elective</span>
+                            </label>
+                        </div>
+                        <div className="flex">
+                            <label className="label cursor-pointer">
+                                <input type="radio" name="elective" className="radio checked:bg-primary mr-2" />
+                                <span className="label-text">Free Elective</span>
+                            </label>
+                        </div>
+                    </div>
+                    <div className="flex-auto">
+                        <H4
+                            style="text-base"
+                            text="Search"
+                        />
+                        <input type="text" placeholder="Type here" className="input input-bordered input-primary rounded-full w-full max-w-xs mt-2 mb-5" />
+                        <div className="card-actions justify-end">
+                            <button className="btn btn-sm btn-outline btn-primary">
+                                <Link href="/program/exchange-study-abroad">Submit</Link>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </>
+    )
+}
+
+export default StudyPlans
