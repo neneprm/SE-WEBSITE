5e8eab0c1f5d4fdca90e06241a135054e9878079
neneprm <s.prima@hotmail.com>
2022-11-17 02:12:44 +0700

    Fix footer year

--- client/pages/components/footer.tsx
+++ client/pages/components/footer.tsx
@@ -1,13 +1,14 @@
 import { H1, P } from "./headers"
+const currentYear = new Date().getFullYear()
 const Footer = () => {
   return (
     <footer className="footer-center md:footer py-10 md:py-20 bg-neutral text-neutral-content mt-auto">
       <div className="mx-4 md:mx-12">
         <H1 style="text-primary font-bold" text="KMITL" />
-        <P
-          text="&copy; 2022 International College, King Mongkut's Institute of Technology
-          Ladkrabang"
-        />
+        <p className="text-sm text-left md:text-justify sm:text-base md:text-lg lg:text-xl 2xl:text-2xl">
+          &copy; {currentYear} International College, King Mongkut's Institute
+          of Technology Ladkrabang
+        </p>
       </div>
     </footer>
   )
