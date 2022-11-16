ecc8fba3f319e844ae9da9763107029aa450121e
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2022-11-16 19:36:39 +0700

    Fix tab

--- pages/admissions/curriculum.tsx
+++ pages/admissions/curriculum.tsx
@@ -1,15 +1,38 @@
 import {H3,H4,P} from "../components/headers"
+import { useState } from "react"
 
+const initialTodos = [
+    { id: 0, title: 'TCAS Admissions', description: "tcas admission" },
+    { id: 1, title: 'Direct Admission', description: "direct admission" },
+    { id: 2, title: 'Application Procedure and Schedule', description: "application"},
+  ];
 const Curriculum = () => {
+    const [toggleState, setToggleState] = useState(initialTodos[0].id);
     return(
         <div>
-            <div className="container mx-auto pb-8">
-                <div className="tabs">
-                    <a className="tab tab-bordered">TCAS Admissions</a> 
-                    <a className="tab tab-bordered tab-active">Direct Admission</a> 
-                    <a className="tab tab-bordered">Application Procedure and Schedule</a>
+            <div className="container mx-auto my-8">
+                <div className="tabs my-8">
+                    
+                    <a className={toggleState === 1 ? "tab tab-bordered tab-active text-accent text-base sm:text-2xl" : "tab tab-bordered text-accent text-base sm:text-2xl"} onClick={() => setToggleState(1)}>
+                            TCAS Admission
+                            
+                    </a>
+                    <a className={toggleState === 2 ? "tab tab-bordered tab-active text-accent text-base sm:text-2xl" : "tab tab-bordered text-accent text-base sm:text-2xl"} onClick={() => setToggleState(2)}>
+                            Direct Admission
+                            
+                    </a>
+                    <a className={toggleState === 3 ? "tab tab-bordered tab-active text-accent text-base sm:text-2xl" : "tab tab-bordered text-accent text-base sm:text-2xl"} onClick={() => setToggleState(3)}>
+                            Application Procedure and Schedule
+                    </a>
                 </div>
-                <H3 style="text-accent" text="TCAS Admission"/>
+
+                <div>
+                    
+                </div>
+                
+                
+                
+                
             </div>
         </div>
     )
