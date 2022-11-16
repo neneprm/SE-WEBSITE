8a7cdd267fcbaf0070de8358736c98aaf89777d5
neneprm <s.prima@hotmail.com>
2022-11-17 06:34:36 +0700

    Add favicon

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -1,17 +1,24 @@
 import "../styles/globals.css"
 import type { AppProps } from "next/app"
+import Head from "next/head"
 import NavBar from "./components/navbar"
 import Footer from "./components/footer"
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <div className="bg-base-100 font-Nunito flex xl:flex flex-col min-h-screen">
-      <div className="mx-8 md:mx-16 lg:mx-28 xl:mx-48 2xl:mx-72 ">
-        <NavBar />
-        <Component {...pageProps} />
+    <>
+      <Head>
+        <title>Software Engineering, KMITL</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <div className="bg-base-100 font-Nunito flex xl:flex flex-col min-h-screen">
+        <div className="mx-8 md:mx-16 lg:mx-28 xl:mx-48 2xl:mx-72 ">
+          <NavBar />
+          <Component {...pageProps} />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
+    </>
   )
 }
 
