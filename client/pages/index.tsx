dea4cd31dc93a70f1c84426d65f81e46024be7b6
neneprm <s.prima@hotmail.com>
2022-11-18 02:32:04 +0700

    Create program section

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -16,7 +16,9 @@ import GlasgowCover from "../pages/assets/glasgow-cover.jpg"
 import ExchangeStudyCover from "../pages/assets/exchange-study-cover.jpg"
 
 import { H1, H3, H4, P } from "./components/headers"
-import Card from "./components/card"
+import Card from "./components/cards/card"
+import SolidButton from "./components/buttons/solid-button"
+import BorderButton from "./components/buttons/border-button"
 
 const Home: NextPage = () => {
   return (
@@ -26,15 +28,16 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      {/* Hero */}
       <section className="hero bg-base-100 min-h-screen 2xl:min-h-max 2xl:p-40">
         <div className="hero-content flex-col lg:flex-row lg:space-x-10 xl:space-x-30 2xl:space-x-36">
           <div className="max-w-xs lg:max-w-lg">
             <Image src={HeroArt} alt="Hero Art" />
           </div>
           <span className="mt-8 md:mt-4 text-center lg:text-right">
-          <div className="w-20 md:w-32 mx-auto lg:mx-0 lg:ml-auto mb-2 md:mb-4">
-            <Image src={SeLogo} alt="SE Logo" />
-          </div>
+            <div className="w-20 md:w-32 mx-auto lg:mx-0 lg:ml-auto mb-2 md:mb-4">
+              <Image src={SeLogo} alt="SE Logo" />
+            </div>
             <H1 style="font-bold text-gradient" text="SOFTWARE" />
             <H1 style="font-bold mb-2 md:mb-6" text="ENGINEERING" />
             <H3 style="mb-3" text="Bachelor of Engineering" />
@@ -44,21 +47,12 @@ const Home: NextPage = () => {
         </div>
       </section>
 
-      {/* <div className='flex justify-center items-center space-x-10'>
-        <Image className="max-w-sm rounded-lg shadow-2xl" src={hero} />
-        <span className='text-center lg:text-right'>
-          <H1 style="" text="SOFTWARE"/>
-          <h1 className='sm:text-2xl md:text-5xl lg:text-8xl font-bold mb-6'>ENGINEERING</h1>
-          <h3 className='sm:text-lg md:text-2xl lg:text-5xl mb-3'>Bachelor of Engineering</h3>
-          <h4 className='sm:text-base md:text-lg lg:text-2xl'>Become professional software developers<br />Change the world with lines of code</h4>
-        </span>
-      </div> */}
+      {/* About SE */}
       <section className="my-8 lg:-mt-8">
         <H4
           style="font-bold text-gradient -mt-16 md:-mt-4 mb-4 2xl:mt-0"
           text="What is Software Engineering?"
         />
-
         <div className={`card bg-base-100 card-bordered shadow-lg max-w-fit`}>
           <div className="card-body items-center text-center">
             <FaLaptopCode className="text-secondary text-4xl " />
@@ -80,10 +74,12 @@ const Home: NextPage = () => {
       {/* <H4 style="font-bold text-gradient mt-4 mb-4" text="Forum" /> */}
       {/* <H4 style="font-bold text-gradient mb-4" text="Blogs" /> */}
 
-      {/* <section className="mb-8">
+      {/* Programs */}
+      {/* Desktop */}
+      <section className="hidden xl:block mb-8">
         <H4 style="font-bold text-gradient mb-4" text="Program" />
-        <div className="card bg-base-100 card-bordered card-body shadow-lg w-full p-12">
-          <div className="flex flex-col md:flex-row justify-center space-y-4 md:space-y-0 md:space-x-4">
+        <div className="card bg-base-100 card-bordered card-body shadow-lg w-full lg:p-12">
+          <div className="flex flex-row justify-center space-x-4">
             <Card
               image={CurriculumCover}
               imageAlt="curriculum-cover"
@@ -109,16 +105,50 @@ const Home: NextPage = () => {
               link="/program/exchange-study"
             />
           </div>
+          <div className="card-actions justify-center mt-4 lg:mt-8">
+            <BorderButton
+              text="More program →"
+              link="/program"
+              style="w-full md:btn-wide"
+            />
+          </div>
         </div>
-      </section> */}
+      </section>
 
+      {/* Mobile */}
+      <section className="mb-8 xl:hidden">
+        <H4 style="font-bold text-gradient mb-4" text="Program" />
+        <div className="flex flex-col space-y-4">
+          <SolidButton
+            style="btn-accent text-base sm:text-lg lg:text-2xl 2xl:text-3xl text-base-100 h-20 justify-start"
+            text="CURRICULUM"
+            link="/program/curriculum"
+            icon={<FaScroll className="text-secondary" />}
+          />
+          <SolidButton
+            style="btn-accent text-base sm:text-lg lg:text-2xl 2xl:text-3xl text-base-100 h-20 justify-start"
+            text="KMITL-GLASGOW"
+            link="/program/kmitl-glasgow"
+            icon={<FaGraduationCap className="text-secondary" />}
+          />
+          <SolidButton
+            style="btn-accent text-base sm:text-lg lg:text-2xl 2xl:text-3xl text-base-100 h-20 justify-start"
+            text="EXCHANGE STUDY"
+            link="/program/exchange-study"
+            icon={<FaPlane className="text-secondary" />}
+          />
+        </div>
+        <div className="card-actions justify-center mt-4 lg:mt-8">
+          <BorderButton
+            text="More program →"
+            link="/program"
+            style="w-full md:btn-wide"
+          />
+        </div>
+      </section>
+
+      {/* Admissions */}
       <H4 style="font-bold text-gradient mb-4" text="Admissions" />
-
-      {/* <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam, repellat! Deserunt eum quae, earum eos distinctio minima a mollitia? Beatae voluptates aliquam commodi ratione quis molestiae magnam vero dolore officiis.
-      </p>
-      <button className='btn bg-gradient'>Hello daisyUI</button>
-      <GradientButton name='Read more →' /> */}
     </>
   )
 }
