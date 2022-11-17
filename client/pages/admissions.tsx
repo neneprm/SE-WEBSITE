4a02a80753d0f613ffcf75674aef790825e61c62
neneprm <s.prima@hotmail.com>
2022-11-17 18:45:34 +0700

    Fix admission UI

--- client/pages/admissions.tsx
+++ client/pages/admissions.tsx
@@ -1,6 +1,8 @@
 import Head from "next/head"
 import { H3 } from "./components/headers"
-import GhostButton from "./components/ghost-button"
+import AdmissionCard from "./components/admission-card"
+import BorderButton from "./components/border-button"
+
 const Admissions = () => {
   return (
     <>
@@ -13,20 +15,31 @@ const Admissions = () => {
         <H3 style="text-gradient font-bold text-center" text="ADMISSIONS" />
       </section>
 
-      <section>{/* Content */}</section>
+      <section className="flex flex-col md:flex-row justify-center space-y-4 md:space-x-4 md:space-y-0 mb-4">
+        <BorderButton
+          text="Apply admission online"
+          link="https://new.reg.kmitl.ac.th/admission"
+        />
+        <BorderButton
+          text="Admission announcement"
+          link="https://new.reg.kmitl.ac.th/admission/#/undergraduate/announcement_inter"
+        />
+      </section>
 
-      {/* <section className="h-56 grid grid-cols-2 gap-40 content-center ...">
-        <Button
-          style="btn-outline border-2 btn-accent"
-          link="/admissions/curriculum"
-          text="CURRICULUM"
-        ></Button>
-        <Button
-          style="btn-outline border-2 btn-accent"
-          link="/admissions/announcement"
-          text="ANNOUNCEMENT"
-        ></Button>
-      </section> */}
+      <section className="mb-8">
+        <AdmissionCard
+          title="Announcement Direct Admissions 1-1 (Early Round)"
+          link="https://reg.kmitl.ac.th/TCAS_old/news/files/2566_1_news1_2397_2022_10_30-20-17-48_c7831.pdf"
+        />
+        <AdmissionCard
+          title="Announcement Direct Admissions 1-1 (Early Round)"
+          link="https://reg.kmitl.ac.th/TCAS_old/news/files/2566_1_news1_2397_2022_10_30-20-17-48_c7831.pdf"
+        />
+        <AdmissionCard
+          title="Announcement Direct Admissions 1-1 (Early Round)"
+          link="https://reg.kmitl.ac.th/TCAS_old/news/files/2566_1_news1_2397_2022_10_30-20-17-48_c7831.pdf"
+        />
+      </section>
     </>
   )
 }
