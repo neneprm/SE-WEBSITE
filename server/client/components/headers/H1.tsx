2524f908e34ca3b5a40aa07f0a7eed8f5a161c5e
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2022-11-20 22:19:12 +0700

    Revert "Fix size"
    This reverts commit 0553ee34a296b51797d5ead8cadaa9051f942b78.

--- server/client/components/headers/H1.tsx
+++ server/client/components/headers/H1.tsx
@@ -1,15 +1,15 @@
 interface IHeader {
-  text: string
-  style?: string
-}
-
+    text: string
+    style?: string
+  }
+  
 const H1: React.FunctionComponent<IHeader> = ({ style, text }) => {
-  return (
-    <h1
-      className={`text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-6xl ${style}`}
-    >
-      {text}
-    </h1>
-  )
+    return (
+      <h1
+        className={`text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-6xl ${style}`}
+      >
+        {text}
+      </h1>
+    )
 }
 export default H1
--- server/client/components/headers/H4.tsx
+++ server/client/components/headers/H4.tsx
@@ -1,15 +1,15 @@
 interface IHeader {
-  text: string
-  style?: string
-}
-
+    text: string
+    style?: string
+  }
+  
 const H4: React.FunctionComponent<IHeader> = ({ style, text }) => {
-  return (
-    <h4
-      className={`text-base sm:text-lg md:text-xl lg:text-2xl 2xl:text-3xl ${style}`}
-    >
-      {text}
-    </h4>
-  )
+    return (
+      <h4
+        className={`text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-6xl ${style}`}
+      >
+        {text}
+      </h4>
+    )
 }
 export default H4
