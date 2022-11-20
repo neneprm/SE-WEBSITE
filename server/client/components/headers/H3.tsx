0553ee34a296b51797d5ead8cadaa9051f942b78
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2022-11-20 22:16:21 +0700

    Fix size

--- server/client/components/headers/H3.tsx
+++ server/client/components/headers/H3.tsx
@@ -1,15 +1,15 @@
 interface IHeader {
-    text: string
-    style?: string
-  }
-  
+  text: string
+  style?: string
+}
+
 const H3: React.FunctionComponent<IHeader> = ({ style, text }) => {
-    return (
-      <h3
-        className={`text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-6xl ${style}`}
-      >
-        {text}
-      </h3>
-    )
+  return (
+    <h3
+      className={`text-lg sm:text-xl md:text-2xl lg:text-3xl 2xl:text-4xl ${style}`}
+    >
+      {text}
+    </h3>
+  )
 }
 export default H3
--- server/client/components/headers/P.tsx
+++ server/client/components/headers/P.tsx
@@ -1,15 +1,15 @@
 interface IHeader {
-    text: string
-    style?: string
-  }
-  
+  text: string
+  style?: string
+}
+
 const P: React.FunctionComponent<IHeader> = ({ style, text }) => {
-    return (
-      <p
-        className={`text-2xl sm:text-3xl md:text-4xl lg:text-5xl 2xl:text-6xl ${style}`}
-      >
-        {text}
-      </p>
-    )
+  return (
+    <p
+      className={`text-sm text-left md:text-justify sm:text-base md:text-lg lg:text-xl 2xl:text-2xl ${style}`}
+    >
+      {text}
+    </p>
+  )
 }
 export default P
