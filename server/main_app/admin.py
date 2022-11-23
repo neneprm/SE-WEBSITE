4010e0848fbc7e0e53ba673dd895ca8c1111e76c
Ratchwalee <88896037+Beammi@users.noreply.github.com>
2022-11-23 15:57:00 +0700

    Add admin site

--- server/main_app/admin.py
+++ server/main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Admission, Subject, About
+
+admin.site.register(Admission)
+admin.site.register(Subject)
+admin.site.register(About)
